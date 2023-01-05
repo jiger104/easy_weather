c4f6c6d8e406521739e1db6e6994609d82a92c16
Jigar <jigar@automationzen.com>
2023-01-05 17:03:53 -0600

    add error handler

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+
+<div class="container h-full mx-auto flex justify-center items-center">
+  <div class="space-y-8 text-center">
+  <h1>{$page.status}</h1>
+  <h2>{$page.error.message + "!"}</h2>
+  </div>
+
+</div>
